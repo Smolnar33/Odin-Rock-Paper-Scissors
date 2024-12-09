d77647cd6b03a5d4a8dbaad5c3b7de66eba5b2c4
Sandor Molnar <mol.sandor.92@gmail.com>
2024-12-09 13:39:11 +0200

    New UI for the game

--- javascript.js
+++ javascript.js
@@ -12,87 +12,77 @@ const getComputerChoice = () => {
   }
 };
 
-const getHumanChoice = () => {
-  let input = prompt(
-    'Please pick: rock , paper or scissor'
-  ).toLowerCase();
-  if (input === 'rock') {
-    return 'rock';
-  } else if (input === 'paper') {
-    return 'paper';
-  } else if (input === 'scissor') {
-    return 'scissor';
-  } else
-    return alert(
-      'You wrote something else. Please write rock , paper or scissors !'
-    );
-};
-
-// let humanSelection = getHumanChoice();
-// let computerSelection = getComputerChoice();
-
 function playRound(humanChoice, computerChoice) {
-  //   console.log(humanChoice, computerChoice);
   if (humanChoice === 'rock' && computerChoice === 'rock')
-    return console.log(
-      `Computer chose -> ${computerChoice} and you choosed ${humanChoice}. It's a tie!`
-    );
+    return (document.querySelector(
+      '.human-choice'
+    ).innerHTML = `You both choosed ${humanChoice}. It's a tie!`);
+
   if (humanChoice === 'paper' && computerChoice === 'paper')
-    return console.log(
-      `Computer chose -> ${computerChoice} and you choosed ${humanChoice}. It's a tie!`
-    );
+    return (document.querySelector(
+      '.human-choice'
+    ).innerHTML = `You both choosed ${humanChoice}. It's a tie!`);
+
   if (humanChoice === 'scissor' && computerChoice === 'scissor')
-    return console.log(
-      `Computer chose -> ${computerChoice} and you choosed ${humanChoice}. It's a tie!`
-    );
+    return (document.querySelector(
+      '.human-choice'
+    ).innerHTML = `You both choosed ${humanChoice}. It's a tie!`);
   if (humanChoice === 'paper' && computerChoice === 'rock') {
     humanScore++;
-    return console.log(
-      `You win ! ${humanChoice} beats ${computerChoice}`
-    );
+    return (document.querySelector(
+      '.human-choice'
+    ).innerHTML = `You win ! ${humanChoice} beats ${computerChoice}`);
   }
   if (humanChoice === 'paper' && computerChoice === 'scissor') {
     computerScore++;
-    return console.log(
-      `You lose ! ${computerChoice} beats ${humanChoice}`
-    );
+    return (document.querySelector(
+      '.human-choice'
+    ).innerHTML = `You lose ! ${computerChoice} beats ${humanChoice}`);
   }
   if (humanChoice === 'rock' && computerChoice === 'paper') {
     humanScore++;
-    return console.log(
-      `You lose ! ${computerChoice} beats ${humanChoice}`
-    );
+    return (document.querySelector(
+      '.human-choice'
+    ).innerHTML = `You lose ! ${computerChoice} beats ${humanChoice}`);
   }
   if (humanChoice === 'rock' && computerChoice === 'scissor') {
     humanScore++;
-    return console.log(
-      `You Win ! ${humanChoice} beats ${computerChoice}`
-    );
+    return (document.querySelector(
+      '.human-choice'
+    ).innerHTML = `You win ! ${humanChoice} beats ${computerChoice}`);
   }
   if (humanChoice === 'scissor' && computerChoice === 'rock') {
     computerScore++;
-    return console.log(
-      `You lose ! ${computerChoice} beats ${humanChoice}`
-    );
+    return (document.querySelector(
+      '.human-choice'
+    ).innerHTML = `You lose ! ${computerChoice} beats ${humanChoice}`);
   }
   if (humanChoice === 'scissor' && computerChoice === 'paper') {
     humanScore++;
-    return console.log(
-      `You Win ! ${humanChoice} beats ${computerChoice}`
-    );
+    return (document.querySelector(
+      '.human-choice'
+    ).innerHTML = `You win ! ${humanChoice} beats ${computerChoice}`);
   }
 }
 
-function playgame() {
-  for (i = 1; i <= 5; i++) {
-    console.log(`Round ${i}`);
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-  }
-  console.log(
-    `Your score -> ${humanScore} | Computer score -> ${computerScore}`
-  );
+function playgame(humanSelection) {
+  let computerSelection = getComputerChoice();
+  playRound(humanSelection, computerSelection);
+
+  document.querySelector(
+    '.human'
+  ).innerHTML = `Human = ${humanScore}`;
+
+  document.querySelector(
+    '.computer'
+  ).innerHTML = `Computer = ${computerScore}`;
+  if (humanScore == 5) alert('You won !');
+  if (computerScore == 5) alert('You lost. The computer won !');
 }
 
-playgame();
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissor = document.querySelector('.scissor');
+rock.addEventListener('click', () => playgame('rock'));
+paper.addEventListener('click', () => playgame('paper'));
+scissor.addEventListener('click', () => playgame('scissor'));
